acee68ae47606d163f9f95d61e2632c9ecd7c5d7
irshad133 <irshad133.ali@gmail.com>
2024-05-15 11:30:17 +0530

    Security Changes

--- src/main/java/com/irshad/config/AppConfig.java
+++ src/main/java/com/irshad/config/AppConfig.java
@@ -4,8 +4,11 @@ import java.util.Arrays;
 import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,26 +22,27 @@ import com.irshad.constant.JwtConstant;
 import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
+@EnableWebSecurity
 public class AppConfig {
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(
 						authorize -> authorize.requestMatchers("/api/**").authenticated().anyRequest().permitAll())
-				.addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class).csrf().disable().cors()
-				.configurationSource(new CorsConfigurationSource() {
+				.addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class).csrf(csrf -> csrf.disable())
+				.cors(cors -> cors.configurationSource(new CorsConfigurationSource() {
 
 					@Override
 					public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 						CorsConfiguration cfg = new CorsConfiguration();
-						cfg.setAllowedOrigins(Arrays.asList("http://localhost:3000/"));
+						cfg.setAllowedOrigins(Arrays.asList("*"));
 						cfg.setAllowedMethods(Collections.singletonList("*"));
 						cfg.setAllowCredentials(true);
 						cfg.setAllowedHeaders(Collections.singletonList("*"));
 						cfg.setExposedHeaders(Arrays.asList(JwtConstant.JWT_HEADER));
 						return cfg;
 					}
-				}).and().formLogin().and().httpBasic();
+				})).formLogin(withDefaults()).httpBasic(withDefaults());
 		return http.build();
 	}
 
